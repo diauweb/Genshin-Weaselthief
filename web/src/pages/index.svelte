a32a6747ec4f2e4429c6af7d5ee4f37bfb1333a0
Xiaodai Dai <diauweb@live.com>
2022-03-23 08:10:51 +0800

    feat: disclaimer

--- web/src/pages/index.svelte
+++ web/src/pages/index.svelte
@@ -17,6 +17,11 @@
 </script>
 
 <p><strong>Proof-Of-Concept project, alpha quality.</strong></p>
+<p style="color: grey; text-size: small;">
+    This project is made for educational and research purpose.<br>
+    This project is not affiliated, associated, authorized, endorsed by, or in any way officially connected with miHoYo, or any of its subsidiaries or its affiliates. <br>
+    Genshin Impact as well as related names, marks, emblems and images are registered trademarks of their respective owners.
+</p>
 
 <div>
     <input type="search" placeholder="Search in mapped texts" bind:value={searchText}>

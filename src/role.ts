2460a14b60e19efe55195f308a9c89f13963b7d4
Xiaodai Dai <diauweb@live.com>
2022-06-27 11:07:46 +0800

    fix: reture correct type

--- src/role.ts
+++ src/role.ts
@@ -1,8 +1,10 @@
 import { currentOid, findOne } from "./db.js";
 
 export async function getNpc (v : string) {
-    return findOne("NPC", {
+    const result = await findOne("NPC", {
         _ver: currentOid(),
         Id: parseInt(v)
     });
+    
+    return { result };
 }
